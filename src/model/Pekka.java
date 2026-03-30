306d4ad1aa9c4872d996ceee014fb4a5f4b4a2d2
BentekhiciFarah <161530919+BentekhiciFarah@users.noreply.github.com>
2026-03-30 15:48:09 +0200

    Bouger Pekka
    Added attributes and methods for manual movement of Pekka.

--- src/model/Pekka.java
+++ src/model/Pekka.java
@@ -2,7 +2,41 @@ package model;
 
 public class Pekka extends Troupe {
 
+    // Attributs de destination (propres au Pekka)
+    private Integer targetX = null;
+    private Integer targetY = null;
+	
+	// Constructeur de Pekka
     public Pekka(int x, int y) {
         super(x, y, 200, 60, 1);
+    }
+
+    // Définir une destination
+    public void setDestination(int x, int y) {
+        this.targetX = x;
+        this.targetY = y;
+
+        // Enlever la cible automatique
+        this.setCible(null);
+    }
+
+    // Override du comportement
+    @Override
+    public void agirManuellement() {
+
+        // Déplacement manuel UNIQUEMENT pour le Pekka
+        if (targetX != null && targetY != null) {
+            if (!isArrived(targetX, targetY)) {
+                moveTo(targetX, targetY);
+            } else {
+                // Arrivé
+                targetX = null;
+                targetY = null;
+            }
+            return;
+        }
+
+        // Sinon comportement normal
+        super.agirManuellement();
     }
 }
